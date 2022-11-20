472207c897653b980eb3522055d8e443c696af1d
CyAaron Tai Nava <cyaaron.tai.nava@gmail.com>
2022-11-19 18:34:57 -0600

    todo add a spot to get /config from api

--- client/src/components/core/ControlPanel.jsx
+++ client/src/components/core/ControlPanel.jsx
@@ -1,4 +1,4 @@
-import { useContext } from 'react';
+import { createContext, useContext } from 'react';
 import { ApiContext } from '../../App';
 import { SendGasForm } from '../wagmi/SendGasForm';
 import { Accordion } from 'react-bootstrap';
@@ -6,42 +6,47 @@ import { BigNumber } from 'ethers';
 import CustomConfig from './CustomConfig';
 import { Cashout } from './Cashout';
 
-export const uint = (bigNumber) => parseInt(bigNumber.toString());
-export const mixed = (res) =>
-  res.map((val) => {
-    if (typeof val === 'object') {
-      if (Array.isArray(val)) return mixed(val);
-      else if (BigNumber.isBigNumber(val)) return uint(val);
-      else return val.toString();
-    }
-    return val;
-  });
-export const contractInterface = (name) =>
-  require(`../wagmi/interfaces/${name}.json`).abi;
+// export const uint = (bigNumber) => parseInt(bigNumber.toString());
+// export const mixed = (res) =>
+//   res.map((val) => {
+//     if (typeof val === 'object') {
+//       if (Array.isArray(val)) return mixed(val);
+//       else if (BigNumber.isBigNumber(val)) return uint(val);
+//       else return val.toString();
+//     }
+//     return val;
+//   });
+// export const contractInterface = (name) =>
+//   require(`../wagmi/interfaces/${name}.json`).abi;
+
+const ConfigContext = createContext(null);
 
 export default function ControlPanel() {
+  let config;
   const { report } = useContext(ApiContext);
 
   return (
-    <Accordion defaultActiveKey={['0']}>
-      <Accordion.Item eventKey="0">
-        <Accordion.Header>General</Accordion.Header>
-        <Accordion.Body>
-          <h6>Administrator</h6>
-          {report.admin}
-          <hr />
-          <Cashout />
-          <hr />
-          <h6>Top Up</h6>
-          <SendGasForm to={report.deployer} />
-        </Accordion.Body>
-      </Accordion.Item>
-      <Accordion.Item eventKey="1">
-        <Accordion.Header>Config</Accordion.Header>
-        <Accordion.Body>
-          <CustomConfig />
-        </Accordion.Body>
-      </Accordion.Item>
-    </Accordion>
+    <ConfigContext.Provider value={{ config }}>
+      <Accordion defaultActiveKey={['0']}>
+        <Accordion.Item eventKey="0">
+          <Accordion.Header>General</Accordion.Header>
+          <Accordion.Body>
+            <h6>Administrator</h6>
+            {report.admin}
+            <hr />
+            <Cashout />
+            <hr />
+            <h6>Top Up</h6>
+            <SendGasForm to={report.deployer} />
+          </Accordion.Body>
+        </Accordion.Item>
+        <Accordion.Item eventKey="1">
+          <Accordion.Header>Config</Accordion.Header>
+          <Accordion.Body>
+            <CustomConfig />
+          </Accordion.Body>
+        </Accordion.Item>
+      </Accordion>
+    </ConfigContext.Provider>
   );
 }
